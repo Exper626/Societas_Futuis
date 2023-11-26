64aec3318dc244f4929a58fb83b7ad31fc1c2bdd
Wolfotex <wolfotex@gmail.com>
2023-11-26 07:25:53 +0530

    user registration and main menu

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        Advisor x = new Advisor("xx", "1234", "john", "doe", "123456", "123@123");
+        Login login = new Login();
 
-        Student y = new Student("xxx", "1234", "yah" , "yeet", "123456" , " 2021/21/2", "2021");
-
-
-
-
-        System.out.println(x.getFirst_name() + " " + x.getLast_name());
-        System.out.println(y.getFirst_name() + " " + y.getLast_name());
+        try{
+            login.loginAuth("a0002","pass123");
+        }
+        catch (Exception e ){
+            System.out.println(e);
+        }
     }
 }
