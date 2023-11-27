8458bf4ddd1ec57d498410b4bb58d96f9e880080
MedhGo <gouri.20210808@iit.ac.lk>
2023-11-27 19:58:25 +0530

    Club related functions and connection to the db

--- Club_Creation/src/ClubFunc.java
+++ Club_Creation/src/ClubFunc.java
@@ -1,37 +1,84 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ClubFunc {
+
+    // add new clubs to database
     public void addClub(Club newClub) throws SQLException {
+        //create sqlConnection object
         SqlConnection sqlConnection = new SqlConnection();
+
+        //establish connection with database
         sqlConnection.startConnection();
+
+        //MySQL query to insert data into database
         String query = "INSERT INTO test.club VALUES" + "('" + newClub.getClub_id() + "', '" + newClub.getClub_name() +
                 "', '" + newClub.getClub_description() + "', true)";
-        System.out.println(query);
+
+        //Execute the query
         sqlConnection.insertData(query);
+
         System.out.println("added");
+
+        //close connection
         sqlConnection.closeConnection();
     }
 
+    // Display all existing clubs
     public void viewAllClubs() throws SQLException {
-        SqlConnection sqlConnection = new SqlConnection();
-        sqlConnection.startConnection();
-        String query = "SELECT * FROM test.club;";
-        ResultSet data = sqlConnection.executeQuery(query);
-        String line = "-".repeat(123);
+        //define variable
+        int num =0;
+
+        //create arrayList to hold the club details
+        ArrayList<Club> clubListArray = getClubs();
+
+        //Display the club details
+        String line = "-".repeat(128);
         System.out.println(line);
-        System.out.format("|%8s | %-30s | %-40s | %-20s | %10s |", "Club ID", "Club Name", "Club Description", "Membership Criteria", "Status");
+        System.out.format("|%3s |%8s | %-30s | %-40s | %-20s | %10s |","No." , "Club ID", "Club Name", "Club Description", "Membership Criteria", "Status");
         System.out.println("");
         System.out.println(line);
 
-        while(data.next()){
+        //loop through all the elements in the clubList array
+        while(clubListArray.size() > num){
+            Club tempClub = new Club(clubListArray.get(num).getClub_id(), clubListArray.get(num).getClub_name(), clubListArray.get(num).getClub_description(), clubListArray.get(num).getMembershipCriteria(), clubListArray.get(num).getClubStatus());
 
-            Club tempClub = new Club(data.getString("club_id"), data.getString("club_name"), data.getString("description"), data.getString("membership_criteria"), data.getString("status"));
-
-            System.out.format("|%8s | %-30s | %-40s | %-20s | %10s |", tempClub.getClub_id(), tempClub.getClub_name(), tempClub.getClub_description(), tempClub.getMembershipCriteria(), tempClub.getClubStatus());
+            System.out.format("|%3s |%8s | %-30s | %-40s | %-20s | %10s |", (num+1), tempClub.getClub_id(), tempClub.getClub_name(), tempClub.getClub_description(), tempClub.getMembershipCriteria(), tempClub.getClubStatus());
             System.out.println();
+            num++;
         }
         System.out.println(line);
+    }
+
+    //get all the existing clubs and clubDetails
+    public ArrayList<Club> getClubs() throws SQLException {
+        //Create sqlConnection obj
+        SqlConnection sqlConnection = new SqlConnection();
+
+        //establish connection with database
+        sqlConnection.startConnection();
+
+        //MySQL query to get all club data from club table
+        String query = "SELECT * FROM test.club;";
+
+        //execute query and store the data in data ResultSet
+        ResultSet data = sqlConnection.executeQuery(query);
+
+        //create an arrayList
+        ArrayList<Club> clubListArray = new ArrayList<>();
+
+        //add clubs to the clubListArray as club obj(s)
+        while (data.next()){
+            //create a temp instance of club
+            Club tempClub = new Club(data.getString("club_id"), data.getString("club_name"), data.getString("description"), data.getString("membership_criteria"), data.getString("status"));
+            //add the club to the arraylist
+            clubListArray.add(tempClub);
+        }
+
+        //return the arraylist
+        return clubListArray;
+
     }
 }
 
--- Club_Creation/src/SqlConnection.java
+++ Club_Creation/src/SqlConnection.java
@@ -1,14 +1,22 @@
 import java.sql.*;
 
 public class SqlConnection {
-    private static String url = "jdbc:mysql://localhost:3306/test";
-    private static String username = "root";
-    private static String password = "password";
     private Connection connection;
 
+    //establish connection with database
     public void startConnection(){
         try {
+            //load mysql jdbc driver class
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //connection url for jdbc
+            String url = "jdbc:mysql://localhost:3306/test";
+
+            //database credentials
+            String username = "root";
+            String password = "password";
+
+            //establish connection with the database
             this.connection = DriverManager.getConnection(url, username, password);
         }
         catch (Exception e){
@@ -17,14 +25,19 @@ public class SqlConnection {
     }
 
     public ResultSet executeQuery(String query) throws SQLException{
+        //create a PreparedStatement for executing the MySQL query
         PreparedStatement preparedStatement = connection.prepareStatement(query);
+        //execute the query and return the result
         return preparedStatement.executeQuery();
     }
 
     public void insertData(String query){
         try {
             //PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            //create a statement for executing the MySQL query
             Statement insertData = connection.createStatement();
+            // Execute the query to insert, update, or delete data
             insertData.executeUpdate(query);
         }
         catch (Exception e){
@@ -33,9 +46,12 @@ public class SqlConnection {
 
     }
 
+    // close connection with the database
     public void closeConnection(){
         try{
+            //check if the connection is not null before attempting to close it
             if (connection != null) {
+                // Close the database connection
                 connection.close();
             }
         }
